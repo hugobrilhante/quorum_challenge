bed65ffaa528231a4e989bb4fb5baeb86174bd99
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-22 18:44:35 -0300

    Add bill serializer

--- backend/src/core/serializers.py
+++ backend/src/core/serializers.py
@@ -4,3 +4,9 @@ from rest_framework import serializers
 class LegislatorSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
+
+
+class BillSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    sponsor_id = serializers.IntegerField()
