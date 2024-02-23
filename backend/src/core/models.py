fadfeded66d0e2e376584c21f05add4c2544e00c
Hugo Brilhante <hugobrilhante@gmail.com>
2024-02-23 09:05:39 -0300

    Add new method filter to BaseModel

--- backend/src/core/models.py
+++ backend/src/core/models.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from typing import List
 
-from src.core.data_sourcers import BillCSVDataSource, VoteCSVDataSource, VoteResultCSVDataSource
+from src.core.data_sourcers import BillCSVDataSource
 from src.core.data_sourcers import LegislatorCSVDataSource
+from src.core.data_sourcers import VoteCSVDataSource
+from src.core.data_sourcers import VoteResultCSVDataSource
 
 DATA_DIR = Path(__file__).resolve().parent / '../../data'
 
@@ -15,6 +17,19 @@ class BaseModel:
     @classmethod
     def all(cls) -> List['BaseModel']:
         return [cls(**item) for item in cls.data_source().get_data()]
+
+    @classmethod
+    def filter(cls, **kwargs) -> List['BaseModel']:
+        items = cls.all()
+        filtered_data = [item for item in items if cls._matches_filters(item, kwargs)]
+        return filtered_data
+
+    @classmethod
+    def _matches_filters(cls, item, filters) -> bool:
+        for key, value in filters.items():
+            if getattr(item, key, None) != value:
+                return False
+        return True
 
 
 class Legislator(BaseModel):
